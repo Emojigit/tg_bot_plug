1bd9b8675f8d7b16698331bbee8093be89aa4787
Emojigit <55009343+Emojigit@users.noreply.github.com>
2022-05-22 15:46:35 +0800

    storage system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, asyncio, importlib
+import sys, asyncio, importlib, json
 from telethon import TelegramClient, functions, types, events
 from telethon.errors import *
 
@@ -16,15 +16,41 @@ except ImportError:
         print("config.example.py not found, make sure you're in the script's directory!")
     exit(1)
 
+class Storage:
+    def __init__(self, name):
+        fname = "data-" + name + ".json"
+        try:
+            self.dataF = open(fname,"r+")
+        except FileNotFoundError:
+            with open(fname, 'w') as tmpF:
+                tmpF.write("{}")
+            self.dataF = open(fname,"r+")
+        self.data = json.load(self.dataF)
+    def get(self,key,default=None):
+        return self.data[key] if key in self.data else default
+    def save(self):
+        self.dataF.seek(0)
+        json.dump(self.data,self.dataF)
+        self.dataF.truncate()
+    def set(self,key,value,autosave=True):
+        if value == None:
+            self.data.pop(key, None)
+        else:
+            self.data[key] = value
+        if autosave:
+            self.save()
+
+
 bot = TelegramClient('bot', config.api_id, config.api_hash).start(bot_token=config.bot_token)
 plugins = [importlib.import_module(plugin) for plugin in config.plugins_enabled]
-for x in plugins:
-    print("Loading plugin: {}".format(x.__name__))
-    x.setup(bot)
+
+
 
 txt_plugslist = []
 txt_plugslist.append("List of plugins installed:")
 for x in plugins:
+    print("Loading plugin: {}".format(x.__name__))
+    # Prepare doc
     dirs = dir(x)
     name = x.__name__
     if "__dname__" in dirs:
@@ -34,6 +60,12 @@ for x in plugins:
     else:
         txt_plugslist.append("{}: {}".format(name,x.__description__))
     txt_plugslist.append("- By {}, ver. {}".format(x.__author__,x.__version__))
+    # Execute script
+    s = Storage(x.__name__)
+    try:
+        x.setup(bot,s)
+    except TypeError: # Legacy plugins without storage field
+        x.setup(bot)
 txt_plugslist = "\n".join(txt_plugslist)
 
 @bot.on(events.NewMessage(pattern='/plugslist'))
--- plugins/start/__init__.py
+++ plugins/start/__init__.py
@@ -5,10 +5,13 @@ __description__ = "Example of plugin"
 __dname__ = "start"
 
 from telethon import events
-def setup(bot):
+def setup(bot,storage):
     @bot.on(events.NewMessage(pattern='/start'))
     async def start(event):
         """Send a message when the command /start is issued."""
-        await event.respond('Hello, I am a bot!\nBot powered by Telegram Bot Plug')
+        executed_times = storage.get("exectimes",0)
+        executed_times += 1
+        await event.respond('Hello, I am a bot!\nBot powered by Telegram Bot Plug\nExecuted: ' + str(executed_times))
+        storage.set("exectimes", executed_times)
         raise events.StopPropagation
 
